987ba421fff8c0e7bfb624cf4ac937047d769cde
MrToph <christoph.michel.1992@gmail.com>
2019-05-05 17:47:54 +0100

    refactor steps into their own files

--- src/commands/apply.js
+++ src/commands/apply.js
@@ -1,22 +1,8 @@
-const path = require(`path`)
 const chalk = require(`chalk`)
 const mapValues = require(`lodash/mapValues`)
-const forEach = require(`lodash/forEach`)
-const eos = require(`../eos`)
 const utils = require(`../utils`)
 const Account = require(`../account`)
-
-const performTransaction = async ({sendTransaction, actions}) => {
-    try {
-        await sendTransaction(actions)
-    } catch (error) {
-        throw new Error(
-            `Error while sending transaction:\n  ${error.message}.\n  Payload: ${JSON.stringify(
-                actions,
-            )}`,
-        )
-    }
-}
+const steps = require(`./steps`)
 
 async function apply(envName, options) {
     const {config, configPath} = utils.getConfig(options.config)
@@ -28,7 +14,6 @@ async function apply(envName, options) {
     }
 
     const env = config[envName]
-    const {api, sendTransaction} = eos.initEos(env)
 
     if (env.accounts) {
         env.accounts = mapValues(
@@ -36,41 +21,11 @@ async function apply(envName, options) {
             (account, accountName) => new Account(accountName, account),
         )
 
-        const accounts = Object.keys(env.accounts).map(accountName => env.accounts[accountName])
-        await Promise.all(accounts.map(account => account.fetch({api})))
-
-        const createdAccounts = []
-        /* eslint-disable no-await-in-loop */
-        for (const account of accounts) {
-            const actions = await account.create({env})
-            if (actions) {
-                await performTransaction({sendTransaction, actions})
-                utils.log(utils.chalk.green(`Account "${account.name}" created.`))
-                createdAccounts.push(account)
-            }
-        }
-        /* eslint-enable no-await-in-loop */
-
-        await Promise.all(createdAccounts.map(account => account.fetch({api, delay: 1100})))
-
-        /* eslint-disable no-await-in-loop */
-        for (const account of accounts) {
-            const actions = await account.updateAuth({env})
-            if (actions) {
-                try {
-                    await performTransaction({sendTransaction, actions})
-                } catch (error) {
-                    throw new Error(
-                        `${error.message}\nNote: Updating permissions requires ${utils.chalk.yellow(
-                            `owner`,
-                        )} permissions. Make sure these are available for signatures.`,
-                    )
-                }
-                utils.log(utils.chalk.green(`Permissions for "${account.name}" updated.`))
-                createdAccounts.push(account)
-            }
-        }
-        /* eslint-enable no-await-in-loop */
+        const {fetchAccounts, createAccounts, updateAuth} = steps.getSteps({env})
+        await fetchAccounts()
+        const createdAccounts = await createAccounts()
+        await fetchAccounts(createdAccounts, 1100)
+        await updateAuth()
     }
 
     utils.log(chalk.green`Success!`)
---
+++ src/commands/steps.js
@@ -0,0 +1,68 @@
+const eos = require(`../eos`)
+const utils = require(`../utils`)
+
+const performTransaction = async ({sendTransaction, actions}) => {
+    try {
+        await sendTransaction(actions)
+    } catch (error) {
+        throw new Error(
+            `Error while sending transaction:\n  ${error.message}.\n  Payload: ${JSON.stringify(
+                actions,
+            )}`,
+        )
+    }
+}
+
+const getSteps = ({env}) => {
+    const {api, sendTransaction} = eos.initEos(env)
+    const accounts = Object.keys(env.accounts).map(accountName => env.accounts[accountName])
+
+    const fetchAccounts = async (accountsToFetch = accounts, delay) =>
+        Promise.all(accountsToFetch.map(account => account.fetch({api, delay})))
+
+    const createAccounts = async () => {
+        const createdAccounts = []
+        /* eslint-disable no-await-in-loop */
+        for (const account of accounts) {
+            const actions = await account.create({env})
+            if (actions) {
+                await performTransaction({sendTransaction, actions})
+                utils.log(utils.chalk.green(`Account "${account.name}" created.`))
+                createdAccounts.push(account)
+            }
+        }
+        /* eslint-enable no-await-in-loop */
+
+        return createdAccounts
+    }
+
+    const updateAuth = async () => {
+        /* eslint-disable no-await-in-loop */
+        for (const account of accounts) {
+            const actions = await account.updateAuth({env})
+            if (actions) {
+                try {
+                    await performTransaction({sendTransaction, actions})
+                } catch (error) {
+                    throw new Error(
+                        `${error.message}\nNote: Updating permissions requires ${utils.chalk.yellow(
+                            `owner`,
+                        )} permissions. Make sure these are available for signatures.`,
+                    )
+                }
+                utils.log(utils.chalk.green(`Permissions for "${account.name}" updated.`))
+            }
+        }
+        /* eslint-enable no-await-in-loop */
+    }
+
+    return {
+        fetchAccounts,
+        createAccounts,
+        updateAuth,
+    }
+}
+
+module.exports = {
+    getSteps,
+}
